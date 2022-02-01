70c4138798b9bc341aac35803dcf8af68b869c36
maklad42 <maklad42+git@gmail.com>
2022-02-01 22:55:59 +0900

    Adds roll() function to generate dice rotations and apply the results to the dice container, the sliders, and the value text boxes. Just need to add the button now. 220201

--- dice/scripts/dice.js
+++ dice/scripts/dice.js
@@ -25,3 +25,26 @@ zSlide.onmousemove = function () {
   zVal.textContent = this.value;
   rotateCnt();
 };
+
+function roll() {
+  // generate value for x, y, and z
+  // multiples of 90
+  let x = 367 - Math.floor(Math.random() * 8 + 1) * 90;
+  let y = -356 + Math.floor(Math.random() * 8 + 1) * 90;
+  let z = 360 - Math.floor(Math.random() * 8 + 1) * 90;
+  console.log(x, y, z);
+
+  // apply these values to the css rotateX/Y/Z
+  const cnt = document.querySelector('.container');
+  cnt.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+
+  // apply these values to the sliders
+  xSlide.value = x;
+  ySlide.value = y;
+  zSlide.value = z;
+
+  // apply these values to the text boxes
+  xVal.textContent = x;
+  yVal.textContent = y;
+  zVal.textContent = z;
+}
